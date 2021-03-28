3b128f2bb4f3ec70df86716e9e09d37e77c84a59
Krzysztof Knasiak <krzysztofknasiak.teb@gmail.com>
2021-03-28 17:24:34 +0200

    Dodaj pliki projektów.

---
+++ Domowy_budzet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domowy_budzet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
