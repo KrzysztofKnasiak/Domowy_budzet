3dcd92af98c5f52d7ed82183fda42d2e0b0fcba5
Krzysztof Knasiak <krzysztofknasiak.teb@gmail.com>
2021-04-07 21:55:10 +0200

    Dodanie pól, właściwości oraz metod

--- Domowy_budzet/Classes/TransactionStorage.cs
+++ Domowy_budzet/Classes/TransactionStorage.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Domowy_budzet.Classes.BuisnessLogic;
 
 namespace Domowy_budzet.Classes
 {
     class TransactionStorage : XmlStorage<TransactionStorage>
     {
+
+        public List<Transaction> transactions;
+
         public override bool InitializeFromObject(TransactionStorage Object)
         {
             throw new NotImplementedException();
--- Domowy_budzet/Classes/UserStorage.cs
+++ Domowy_budzet/Classes/UserStorage.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Domowy_budzet.Classes.BuisnessLogic;
 
 namespace Domowy_budzet.Classes
 {
     class UserStorage : XmlStorage<UserStorage>
     {
+        public List<User> users;
         public override bool InitializeFromObject(UserStorage Object)
         {
             throw new NotImplementedException();
